9a2c9af152a58bdaae340e6ddcffba34d545023e
TanyaHamster <tanya.kucheba@gmail.com>
2023-04-13 10:09:29 +0200

    feat: solve dns-starts

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+  
+  domains.forEach(domain => {
+    const subDomains = domain.split('.').reverse();
+    let dns = '';
+    
+    subDomains.forEach(subDomain => {
+      dns += `.${subDomain}`;
+      
+      if (!dnsStats[dns]) {
+        dnsStats[dns] = 1;
+      } else {
+        dnsStats[dns]++;
+      }
+    });
+  });
+  
+  return dnsStats;
 }
 
 module.exports = {
